af76636f1ccb80470e8f761c39482085310fe5e4
Isen <isenkasa@gmail.com>
2021-01-13 10:47:14 -0600

    File Org

---
+++ facebook-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Sidebar />
+      {/* feed */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
